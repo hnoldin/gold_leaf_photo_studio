d326e011d9f65de7a2b76b4d0cf28ad75145b2e8
Hunter Noldin <129526905+hnoldin@users.noreply.github.com>
2024-02-22 13:06:47 -0500

    add lenis smooth scroll

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-console.log('hello');
+console.log('lenis');
 
-import Splide from '@splidejs/splide';
+import Lenis from '@studio-freight/lenis'
+
+// LENIS SMOOTH SCROLL
+let lenis;
+if (Webflow.env("editor") === undefined) {
+  lenis = new Lenis({
+    lerp: 0.1,
+    wheelMultiplier: 0.7,
+    gestureOrientation: "vertical",
+    normalizeWheel: false,
+    smoothTouch: false
+  });
+  function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+}
+$("[data-lenis-start]").on("click", function () {
+  lenis.start();
+});
+$("[data-lenis-stop]").on("click", function () {
+  lenis.stop();
+});
+$("[data-lenis-toggle]").on("click", function () {
+  $(this).toggleClass("stop-scroll");
+  if ($(this).hasClass("stop-scroll")) {
+    lenis.stop();
+  } else {
+    lenis.start();
+  }
+});
 
